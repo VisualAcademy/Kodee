5afcb479cf1e7788be030f5e4acd8ad77d786401
Park YongJune <redplus@redplus.net>
2024-11-29 11:45:59 +0900

    Employee model class file add

---
+++ src/EmployeePicture/Kodee/Kodee.ApiService/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Kodee.ApiService.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+    // TODO: 
+
+}

8f4b860081bb0a65f3d8f14925943b77349e266d
Park YongJune <redplus@redplus.net>
2024-11-29 13:59:56 +0900

    EmployeePhotoDbContext class file add

---
+++ src/EmployeePicture/Kodee/Kodee.ApiService/Models/EmployeePhotoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kodee.ApiService.Models
+{
+    public class EmployeePhotoDbContext : DbContext
+    {
+        public EmployeePhotoDbContext() : base()
+        {
+            
+        }
+
+        public EmployeePhotoDbContext(DbContextOptions<EmployeePhotoDbContext> options)
+            : base(options) 
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}

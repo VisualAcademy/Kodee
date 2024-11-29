2b53dab3ac326a9b64bd23914b61a135feb33fec
Park YongJune <redplus@redplus.net>
2024-11-30 07:45:05 +0900

    .

--- src/EmployeePicture/Kodee/Kodee.ApiService/Controllers/EmployeeController.cs
+++ src/EmployeePicture/Kodee/Kodee.ApiService/Controllers/EmployeeController.cs
@@ -19,18 +19,22 @@ namespace Kodee.ApiService.Controllers
             _context = context;
         }
 
+        #region GetAll
         // GET: api/Employee
+        // 모든 직원 목록을 조회
         [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<EmployeeViewModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IEnumerable<EmployeeViewModel>), StatusCodes.Status200OK)] // 성공 시 반환 타입과 HTTP 상태 코드를 명시
         public async Task<IActionResult> Get()
         {
+            // 직원 데이터를 EmployeeViewModel로 변환하여 반환
             var employees = await _context.Employees
                 //.Select(e => new EmployeeViewModel(e.Id, e.FirstName, e.LastName))
                 .ProjectToType<EmployeeViewModel>()
                 .ToListAsync();
 
             return Ok(employees);
-        }
+        } 
+        #endregion
 
         // GET api/<EmployeeController>/5
         [HttpGet("{id}")]

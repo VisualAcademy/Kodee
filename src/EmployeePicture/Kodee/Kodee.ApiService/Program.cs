cec2fa987e57762a2a763e08b45301142a05f953
Park YongJune <redplus@redplus.net>
2024-11-29 11:40:22 +0900

    Code Cleanup - Remove weatherforecast API.

--- src/EmployeePicture/Kodee/Kodee.ApiService/Program.cs
+++ src/EmployeePicture/Kodee/Kodee.ApiService/Program.cs
@@ -33,22 +33,6 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/openapi/v1.json", "weather api"));
 }
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.UseCors("AllowAllOrigins");
 
 app.UseHttpsRedirection();
@@ -60,8 +44,3 @@ app.MapDefaultEndpoints();
 app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

ca3a4b1722668c6acd1d41bfd420f12cbfd3f3a0
Park YongJune <redplus@redplus.net>
2024-11-29 14:49:19 +0900

    EmployeeViewModel and PhotoViewModel ViewModel Record add

---
+++ src/EmployeePicture/Kodee/Kodee.ApiService/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,3 @@
+﻿namespace Kodee.ApiService.ViewModels;
+
+public record EmployeeViewModel(long Id, string FirstName, string LastName);
---
+++ src/EmployeePicture/Kodee/Kodee.ApiService/ViewModels/PhotoViewModel.cs
@@ -0,0 +1,3 @@
+﻿namespace Kodee.ApiService.ViewModels;
+
+public record PhotoViewModel(long Id, string FileName, long? EmployeeId);
